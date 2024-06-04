7949df9cfac9da4ea00958b71ec0160da218b099
Anatolii Sakhnik <sakhnik@gmail.com>
2024-06-04 17:22:27 +0300

    Fix tags, add code change for consistency

--- lua/make-async.lua
+++ lua/make-async.lua
@@ -161,20 +161,20 @@ local function qf_append(title, lines)
   vim.fn.setqflist({}, "a", { title = title, lines = lines, })
 end
 
+MakeAsync.qf = MakeAsync.new(qf_clear, qf_append)
+
+function MakeAsync.make()
+  MakeAsync.qf:execute(function()
+    local makeprg = vim.o.makeprg
+    if not makeprg then return "" end
+    return vim.fn.expandcmd(makeprg)
+  end)
+end
+
 function MakeAsync.setup()
-  local qf = MakeAsync.new(qf_clear, qf_append)
-
-  local function make()
-    qf:execute(function()
-      local makeprg = vim.o.makeprg
-      if not makeprg then return "" end
-      return vim.fn.expandcmd(makeprg)
-    end)
-  end
-
-  vim.api.nvim_set_keymap('n', '<leader>mm', '', { noremap = true, callback = make, desc = "Run 'makeprg' asynchronously and populate quickfix" })
+  vim.api.nvim_set_keymap('n', '<leader>mm', '', { noremap = true, callback = MakeAsync.make, desc = "Run 'makeprg' asynchronously and populate quickfix" })
   vim.api.nvim_create_user_command('X',
-    function(a) qf:execute(function() return a.args end) end,
+    function(a) MakeAsync.qf:execute(function() return a.args end) end,
     {nargs = "+", complete = "shellcmd", force = true, desc = 'Execute command asynchronously'})
 end
 
